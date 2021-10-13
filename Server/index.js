5e81efca9216676dd5569e4f7d3a29972158b16e
Elwan Mayencourt <elwan.mayencourt@studentfr.ch>
2021-10-13 18:42:50 +0200

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,35 @@
 /**
- * Simple webservice that create carbon image by one request
+ * Simple webservice that create carbon image using one request
  * @author : Elwan Mayencourt
  * @date : 11.10.2021
  */
+
+
+//Requires
 const puppeteer = require('puppeteer')
 const express = require('express')
-const app = express()
-const port = 3000
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid')
 
+//App config
+const app = express()
+const port = 3000
+
+
+//PATH
 const CARBON_URL = `https://carbon.now.sh/?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&ds=true&dsyoff=0px&dsblur=68px&wc=true&wa=true&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false`
 const DOWNLOAD_PATH = '/home/ubuntu/Downloads/carbon.png'
 const PROJECT_PATH = '/home/ubuntu/NodeJs/CTC/'
 const SERVER_URL = 'http://195.15.240.106:3000/'
 
+//Delay function
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 let browser
 let page
 
+
+//Auto run puppeteer
 (async () => {
   browser = await puppeteer.launch({ headless: false })
   page = await browser.newPage()
@@ -56,11 +66,14 @@ app.get('/', (req, res) => {
           break
       }
 
+      //Open carbon with given code and programming language  
       await page.goto(
         `${CARBON_URL}&l=${extension}&code=${encodeURIComponent(
           req.query.code,
         )}`,
       )
+
+      //Export the code into an image
       let selector = '#export-menu'
       await page.waitForSelector(selector)
       await page.click(selector)
